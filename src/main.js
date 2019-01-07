496b8b554561af76d75b1367ea5d3ef28b6fb3a3
lwc <wence.lv@cnlaunch.com>
2019-01-07 17:06:05 +0800

    登录接口对接跳转等

--- src/main.js
+++ src/main.js
@@ -4,27 +4,33 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex';
+
 Vue.use(Vuex);
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.use(ElementUI);
 
 import '@/common/js/validate.js'
 
 import axios from 'axios';
+
 Vue.prototype.$axios = axios;
 
 import {baseURL,} from '@/common/js/public.js';
+
 Vue.prototype.$baseURL = baseURL;
 
 const querystring = require('querystring');
 Vue.prototype.$querystring = querystring;
 
 import _ from 'lodash';
+
 Vue.prototype.$_ = _;
 
 import utils from '@/common/js/utils';
+
 Vue.prototype.$utils = utils;
 
 import '../static/UE/ueditor.config.js'
