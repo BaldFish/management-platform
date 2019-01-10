e5363f0a3de90b1266b353dd4a5850b6e55c6819
lwc <wence.lv@cnlaunch.com>
2019-01-10 18:17:51 +0800

    首页接口对接

--- src/common/js/public.js
+++ src/common/js/public.js
@@ -1,4 +1,4 @@
-let baseURL="http://wallet-api-test.launchain.org:50000";
+let baseURL="https://wallet-api-test.launchain.org";
 let loginPlatform="https://login.datajs.com.cn";
 let exchangePlatform="https://exchange.datajs.com.cn";
 let transferPlatform="https://transfer.datajs.com.cn";
